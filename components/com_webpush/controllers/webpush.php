3862a102d7f5326f657f5adc9be434813947c29c
Dennis Mwea <mweadennis2@gmail.com>
2023-05-04 13:52:48 +0300

    Frontend component logic progress

---
+++ components/com_webpush/controllers/webpush.php
@@ -0,0 +1,51 @@
+<?php
+
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.application.component.controller');
+
+require_once JPATH_COMPONENT . '/models/webpush.php';
+require_once JPATH_COMPONENT . '/models/subscription.php';
+
+class WebpushController extends JControllerLegacy
+{
+	public function getSubscribers(): void
+	{
+		$user              = JFactory::getUser();
+		$response          = new stdClass();
+		$response->success = true;
+		$response->users   = (new SubscriptionModel)->getSubscribers($user);
+
+		echo json_encode($response);
+		jexit();
+	}
+
+	/**
+	 * @return void
+	 * @throws Exception
+	 */
+	public function subscribe(): void
+	{
+		$app = JFactory::getApplication();
+
+		$user     = JFactory::getUser();
+		$model    = new SubscriptionModel;
+		$key      = $app->input->getString('key');
+		$token    = $app->input->getString('token');
+		$state    = $app->input->getString('state');
+		$endpoint = $app->input->getString('endpoint');
+
+		$subscription           = match ($state)
+		{
+			'delete' => $model->delete($endpoint),
+			'create' => $model->create($user, $endpoint, $token, $key),
+			'update' => $model->update($user, $endpoint, $token, $key),
+		};
+		$response               = new stdClass();
+		$response->success      = true;
+		$response->subscription = $subscription;
+
+		echo json_encode($response);
+		$app->close();
+	}
+}
---
+++ components/com_webpush/models/subscription.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
+use Joomla\CMS\User\User;
+
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.application.component.model');
+
+class SubscriptionModel extends JModelLegacy
+{
+	private JDatabaseDriver $db;
+
+	public function __construct($config = array(), MVCFactoryInterface $factory = null)
+	{
+		$this->db = JFactory::getDbo();
+		parent::__construct($config, $factory);
+	}
+
+	public function create(User $user, string $endpoint, string $token, string $key): stdClass
+	{
+		if (!is_null($this->find($endpoint)))
+		{
+			return $this->update($user, $endpoint, $token, $key);
+		}
+
+		$subscription                    = new stdClass();
+		$subscription->public_key        = $key;
+		$subscription->auth_token        = $token;
+		$subscription->content_encoding  = null;
+		$subscription->subscribable_id   = $user->id;
+		$subscription->subscribable_type = get_class($user);
+		$subscription->endpoint          = $endpoint;
+
+		$this->db->insertObject('#__webpush_subscribers', $subscription, 'id');
+
+		return $this->find($endpoint);
+	}
+
+	private function find(string $endpoint): ?stdClass
+	{
+		$query = $this->db->getQuery(true)
+			->select('*')
+			->from($this->db->quoteName('#__webpush_subscribers'))
+			->where($this->db->quoteName('endpoint') . ' = ' . $this->db->quote($endpoint));
+
+		return $this->db->setQuery($query)->loadObject();
+	}
+
+	public function delete($endpoint): bool
+	{
+		$query = $this->db->getQuery(true)
+			->delete('#__webpush_subscribers')
+			->where($this->db->quoteName('endpoint') . ' = ' . $this->db->quote($endpoint));
+
+		return $this->db->setQuery($query)->execute();
+	}
+
+	public function update(User $user, string $endpoint, string $token, string $key): stdClass
+	{
+		$result = $this->find($endpoint);
+
+		if (is_null($result))
+		{
+			return $this->create($user, $endpoint, $token, $key);
+		}
+
+		$subscription             = new stdClass();
+		$subscription->public_key = $key;
+		$subscription->auth_token = $token;
+		$subscription->id         = $result->id;
+		$subscription->subscribable_id   = $user->id;
+
+		$this->db->updateObject('#__webpush_subscribers', $subscription, 'id');
+
+		return $this->find($endpoint);
+	}
+}
