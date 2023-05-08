b13a2f4af500e1841aeed7ae26163c1b4e008f52
Dennis Mwea <mweadennis2@gmail.com>
2023-05-08 20:48:36 +0300

    Fix type

--- tmp/webpush/com_webpush/admin/webpush.php
+++ tmp/webpush/com_webpush/admin/webpush.php
@@ -7,7 +7,7 @@ JFactory::getDocument()
 	->addStyleDeclaration('.icon-webpush {background-image: url(../media/com_webpush/images/notifications.png)}');
 
 // Access check: is this user allowed to access the backend of this component?
-if (!JFactory::getUser()->authorise('core.manage', 'cm_webpush'))
+if (!JFactory::getUser()->authorise('core.manage', 'com_webpush'))
 {
 	throw new Exception(JText::_('JERROR_ALERTNOAUTHOR'));
 }
