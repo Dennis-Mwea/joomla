5070829a48acb21343717e88f58166285956cb6c
Dennis Mwea <mweadennis2@gmail.com>
2023-05-08 09:38:41 +0300

    Minor bug fixes

--- tmp/webpush/com_webpush/media/pwa/meta.php
+++ tmp/webpush/com_webpush/media/pwa/meta.php
@@ -1,50 +1,50 @@
 <!-- Web Application Manifest -->
-<link rel="manifest" href="<?php echo JUri::root(); ?>media/pwa/manifest.json">
+<link rel="manifest" href="<?php echo JUri::root(); ?>media/com_webpush/pwa/manifest.json">
 <!-- Chrome for Android theme color -->
 <meta name="theme-color" content="#ffffff">
 
 <!-- Add to home screen for Chrome on Android -->
 <meta name="mobile-web-app-capable" content="yes">
 <meta name="application-name" content="DropExpress">
-<link rel="icon" sizes="512x512" href="<?php echo JUri::root(); ?>media/pwa/icons/icon-512x512.png">
+<link rel="icon" sizes="512x512" href="<?php echo JUri::root(); ?>media/com_webpush/pwa/icons/icon-512x512.png">
 
 <!-- Add to home screen for Safari on iOS -->
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="black">
 <meta name="apple-mobile-web-app-title" content="DropExpress">
-<link rel="apple-touch-icon" href="<?php echo JUri::root(); ?>media/pwa/icons/icon-512x512.png">
+<link rel="apple-touch-icon" href="<?php echo JUri::root(); ?>media/com_webpush/pwa/icons/icon-512x512.png">
 
-<link href="<?php echo JUri::root(); ?>media/pwa/splash/splash-640x1136.png"
+<link href="<?php echo JUri::root(); ?>media/com_webpush/pwa/splash/splash-640x1136.png"
       media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
       rel="apple-touch-startup-image"/>
-<link href="<?php echo JUri::root(); ?>media/pwa/splash/splash-750x1334.png"
+<link href="<?php echo JUri::root(); ?>media/com_webpush/pwa/splash/splash-750x1334.png"
       media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
       rel="apple-touch-startup-image"/>
-<link href="<?php echo JUri::root(); ?>media/pwa/splash/splash-1242x2208.png"
+<link href="<?php echo JUri::root(); ?>media/com_webpush/pwa/splash/splash-1242x2208.png"
       media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
       rel="apple-touch-startup-image"/>
-<link href="<?php echo JUri::root(); ?>media/pwa/splash/splash-1125x2436.png"
+<link href="<?php echo JUri::root(); ?>media/com_webpush/pwa/splash/splash-1125x2436.png"
       media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
       rel="apple-touch-startup-image"/>
-<link href="<?php echo JUri::root(); ?>media/pwa/splash/splash-828x1792.png"
+<link href="<?php echo JUri::root(); ?>media/com_webpush/pwa/splash/splash-828x1792.png"
       media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
       rel="apple-touch-startup-image"/>
-<link href="<?php echo JUri::root(); ?>media/pwa/splash/splash-1242x2688.png"
+<link href="<?php echo JUri::root(); ?>media/com_webpush/pwa/splash/splash-1242x2688.png"
       media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
       rel="apple-touch-startup-image"/>
-<link href="<?php echo JUri::root(); ?>media/pwa/splash/splash-1536x2048.png"
+<link href="<?php echo JUri::root(); ?>media/com_webpush/pwa/splash/splash-1536x2048.png"
       media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
       rel="apple-touch-startup-image"/>
-<link href="<?php echo JUri::root(); ?>media/pwa/splash/splash-1668x2224.png"
+<link href="<?php echo JUri::root(); ?>media/com_webpush/pwa/splash/splash-1668x2224.png"
       media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
       rel="apple-touch-startup-image"/>
-<link href="<?php echo JUri::root(); ?>media/pwa/splash/splash-1668x2388.png"
+<link href="<?php echo JUri::root(); ?>media/com_webpush/pwa/splash/splash-1668x2388.png"
       media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
       rel="apple-touch-startup-image"/>
-<link href="<?php echo JUri::root(); ?>media/pwa/splash/splash-2048x2732.png"
+<link href="<?php echo JUri::root(); ?>media/com_webpush/pwa/splash/splash-2048x2732.png"
       media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
       rel="apple-touch-startup-image"/>
 
 <!-- Tile for Win8 -->
 <meta name="msapplication-TileColor" content="#da532c">
-<meta name="msapplication-TileImage" content="<?php echo JUri::root(); ?>media/pwa/icons/icon-512x512.png">
+<meta name="msapplication-TileImage" content="<?php echo JUri::root(); ?>media/com_webpush/pwa/icons/icon-512x512.png">
