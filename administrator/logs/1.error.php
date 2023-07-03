712a8d6876a34bf68d66340be71a91dae1872382
Auto Commit <contact@synt.ax>
2023-07-03 06:02:20 +0000

    Client amendment (03/07/2023 06:02:20)

--- administrator/logs/1.error.php
+++
@@ -1,9 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2023-05-03 09:19:24 UTC
-#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
-
-#Fields: datetime	priority clientip	category	message
-2023-05-03T09:19:24+00:00	INFO 192.168.64.1	joomlafailure	Empty password not allowed.
-2023-05-03T17:17:38+00:00	INFO 105.163.1.136	joomlafailure	Empty password not allowed.
-2023-05-15T10:39:53+00:00	INFO 197.248.123.119	cookiefailure	Empty password not allowed.
