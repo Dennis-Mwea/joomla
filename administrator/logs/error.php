a5aeff3967b6eead3be4e3ed0edd833581e72d65
Auto Commit <contact@synt.ax>
2023-09-21 18:38:19 +0000

    Client amendment (21/09/2023 18:38:18)

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2023-09-21 18:38:16 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2023-09-21T18:38:16+00:00	INFO 79.217.250.212	joomlafailure	Username and password do not match or you do not have an account yet.
